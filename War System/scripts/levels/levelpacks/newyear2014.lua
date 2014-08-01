ea9a08b47edc62bccf6bf1dbd1048d9ef6c9b346
DarkSline <slava982007@gmail.com>
2014-08-01 13:46:01 +0300

    Trash added.

---
+++ War System/scripts/levels/levelpacks/newyear2014.lua
@@ -0,0 +1,6 @@
+main.levelpack.test = {
+							name   = "New Year 2014",
+							description = "С Наступившим 2014-ым, друзья!/nВ качестве подарка решил я сделать сию кампанию. Удачи!",
+							type   = "single",
+							level1 = "newyear2014",
+							}

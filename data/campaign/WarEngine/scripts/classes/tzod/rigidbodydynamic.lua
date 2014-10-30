d65112ec8d9a4dbb82ca902647acae930e4360d7
DarkSline <slava982007@gmail.com>
2014-10-31 01:01:24 +0300

    Ability to copy direction of followed objects added to RigidBodyDinamic and Sprite.

--- data/campaign/WarEngine/scripts/classes/tzod/rigidbodydynamic.lua
+++ data/campaign/WarEngine/scripts/classes/tzod/rigidbodydynamic.lua
@@ -13,3 +13,34 @@ function RigidBodyDynamic:initialize(name, pos, props)
 
 	
 end
+
+function RigidBodyDynamic:follow(whoName, speed, iteration, copyDir) -- speed here and above in px/sec
+	if iteration == 0 then
+		dbg.Print(self._name.."follow()", "objects")
+		self._allowMoving = true
+	end
+	self._isMoving = true
+
+	pushcmd( function()
+		local whoX, whoY = position(whoName)
+		local distance, angle = self:getVector({ x = whoX, y = whoY }) -- rework access to position (maybe)
+		if distance > 8*32 then
+			self._pos.x = self._pos.x + math.cos(angle)*speed/100
+			self._pos.y = self._pos.y + math.sin(angle)*speed/100
+			self:_updatePos()
+		end
+
+		if copyDir then
+			self._props["dir"] = object(whoName).dir;
+		end
+		
+		self._isMoving = false
+
+		if self._allowMoving then
+			self:follow(whoName, speed, 1) -- recursion
+		end
+
+	end, 1/100)
+
+	return nil
+end
